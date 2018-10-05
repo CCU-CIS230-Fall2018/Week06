4920dc2e186f39c083d288d4369a04483a8db061
Keaton Helm <khelm@students.ccu.edu>
2018-10-04 22:56:48 -0600

    Ready for review.

---
+++ PhoneNumberTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PhoneNumberTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("PhoneNumberTests")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("67e64b3b-8bde-4b2b-a581-852216f134bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Week06/USPhoneNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Week06
+{
+    public class USPhoneNumber
+    {
+        public string Digits { get; set; }
+        public bool ValidTicker { get; set; }
+
+        Regex rgx = new Regex(@"^(\([0-9]{3}\)|[0-9]{3})?[ -\.]?[0-9]{3}[ -\.]?[0-9]{4}$");
+
+        public USPhoneNumber(string digits)
+        {
+            try
+            {
+                if (rgx.IsMatch(digits) == true)
+                {
+                    Digits = digits;
+                    ValidTicker = true;
+                }
+            }
+            catch
+            {
+                if (rgx.IsMatch(digits) == false)
+                {
+                    ValidTicker = false;
+                }
+                else if (digits == "" || digits == null)
+                {
+                    throw new System.ArgumentNullException("This arguement may not be null!");
+                }
+            }
+        }
+    }
+}
