835ced68803eb4a8a614af823e778454fdfb9723
abrilmunoz <42852207+abrilmunoz@users.noreply.github.com>
2018-10-04 19:48:48 -0700

    Update Program.cs

--- Week06/Program.cs
+++ Week06/Program.cs
@@ -10,6 +10,20 @@ namespace Week06
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter a phone number.");
+            string telNo = Console.ReadLine();
+
+            if (Regex.Match(telNo, @"^(\+[0-9])$").Success)
+            {
+                Console.WriteLine("correctly entered");
+            }
+
+            else
+            {
+                Console.WriteLine("incorrectly entered");
+            }
+
+            Console.ReadLine();
         }
     }
 }
